4ed3692dfab95d60be185560fd0333156058ee6c
fcemtopall <f.c.topal@gmail.com>
2021-07-09 12:12:46 +0300

    First Commit

---
+++ app/src/main/java/com/fcemtopal/patika/OnBoardingScreen.kt
@@ -0,0 +1,11 @@
+package com.fcemtopal.patika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
